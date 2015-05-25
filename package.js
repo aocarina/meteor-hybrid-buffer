a5004a920d56e52f4fb221b87f1b613df6b3dabc
Alan Sikora <alan@aocarina.com>
2015-05-25 17:30:34 -0300

    changing repo url to https

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'ocarina:hybrid-buffer',
-  version: '0.5.1',
+  version: '0.5.2',
   // Brief, one-line summary of the package.
   summary: 'A simple way to store buffers that are readable both client and server side',
   // URL to the Git repository containing the source code for this package.
-  git: 'git@github.com:aocarina/meteor-hybrid-buffer.git',
+  git: 'https://github.com/aocarina/meteor-hybrid-buffer.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
