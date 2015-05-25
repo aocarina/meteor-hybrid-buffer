cf51204d14b66912a42ccc36bbc78509334246fb
Alan Sikora <alan@aocarina.com>
2015-05-25 19:56:42 -0300

    added transform method

--- hybrid-buffer.js
+++ hybrid-buffer.js
@@ -9,3 +9,14 @@ HybridBuffer = function HybridBuffer( data ) {
     this._buffer = new Buffer( data );
   }
 }
+
+HybridBuffer.transform = function( hybridBuffer ) {
+  var collection = this;
+  
+  if ( Meteor.isClient ) {
+    return String.fromCharCode.apply( null, hybridBuffer._buffer );
+  } else {
+    return new Buffer( hybridBuffer._buffer );
+  }
+
+};
